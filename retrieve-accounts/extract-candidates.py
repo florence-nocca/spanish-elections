90659794de804f7973ee780c5a6e661f10208229
Florence Nocca <florence.nocca@yahoo.fr>
2015-11-22 19:24:11 +0100

    Creating a csv candidates database from a xml page

---
+++ retrieve-accounts/extract-candidates.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Paquets
+import codecs
+from pyquery import PyQuery as pq
+
+def xmltoDatabase():
+# Open xml list and future candidates database .csv
+    candidates = codecs.open("candidates-database.csv", "w", "utf-8")
+    page = pq(filename = "candidates-list.xml")
+    district = ""
+    election_type = ""
+    party = ""
+    party_ab = ""
+    position = ""
+    sex = ""
+    substitute = False
+
+# extracting informations
+    for line in page("texto p"):
+        name = 0
+        if not line.text:
+            continue
+        if line.text[:5] == "JUNTA":
+            district = " ".join(line.text.strip().split(" ")[3:])
+        if line.text[:8] == "CONGRESO":
+            election_type = "congress"
+        if line.text[:6] == "SENADO":
+            election_type = "senate"
+        if pq(line).attr("class") == "centro_redonda":
+            party = line.text.split("(")[0]
+            party = " ".join(party.strip().split(" ")[1:])
+            substitute = False
+            if "(" in line.text:
+                party_ab = line.text.strip().split("(")[1][:-1]
+        if pq(line).attr("class")[:7] == "parrafo" and line.text[0] in "0123456789":
+            position = line.text.strip().split(" ")[0][:-1]
+            if line.text.split(" ")[1] == "Don":
+                sex = 0
+            else:
+                sex = 1
+            name = " ".join(line.text.strip().split(" ")[2:][:-1])
+            if "Independiente" in name:
+                party = "Independiente"
+        if line.text[:8] == "Suplente": 
+            substitute = True
+        # writing informations in database
+        if substitute == False:
+            name = 0
+        if checkParties(party_ab) == True and name != 0:
+            candidates.write(('"%s","%s","%s","%s","%s","%s","%s"\n') % (name, sex, election_type, district, party, party_ab, position))
+            
+def checkParties(party):
+    tab = [u"C's",u"ERC",u"IU",u"UNIO.CAT",u"PNV",u"PODEMOS",u"PP",u"PS",u"UPyD"]
+    for line in tab:
+        if line in party:
+            return True
+    return False
+
+xmltoDatabase()

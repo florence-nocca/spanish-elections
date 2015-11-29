731b9bc941227d9eb46ccd1568b0e829e62e41ea
Florence Nocca <florence.nocca@yahoo.fr>
2015-11-29 22:33:49 +0100

    New ways to find candidates' accounts were implemented (search on ddg.gg, Twitter and Google)

--- retrieve-accounts/retrieve-accounts.py
+++ retrieve-accounts/retrieve-accounts.py
@@ -2,52 +2,138 @@
 # -*- coding: utf-8 -*-
 
 # Packages
+from pyquery import PyQuery as pq
+import itertools
+import urllib2
 import codecs
 import time
-import calendar
-import urllib2
-from pyquery import PyQuery as pq
+import sys
+import re
 
-# Retrieving candidates' pseudo on Twitter from the csv list
-def retrievePseudo():
-    candidates = codecs.open("candidates-database.csv", "r", "utf-8")
-    accounts = codecs.open("twitter-accounts", "w", "utf-8")
-    results = ""
-    # Creating a table containing candidates' names from the csv list
-    index = 0
-    # Skipping the first line (header)
-    for line in candidates:
-        if index == 0:
-            index +=1
+if len(sys.argv) <= 2:
+    print "Usage: %s <database.csv> <accounts_list.txt>" % sys.argv[0]
+    exit()
+
+def retrieveAccounts():
+    database = [l.strip() for l in codecs.open(sys.argv[1])][1:]
+    accounts = codecs.open(sys.argv[2], "a+")
+    accounts_lines = [l.strip() for l in accounts]
+    previous_name = ""
+    
+    if len(accounts_lines):
+        previous_name = accounts_lines[-1].split(",")[0]
+        
+    for line in database:
+    # Retrieve columns
+        columns = line.strip().split('","')
+        names = columns[0][1:].split(" ")
+
+        district = columns[3]
+        party = columns[5]
+        if party == "C's":
+            party = "Ciudadanos"
+
+        names = cleanNames(names)
+        if previous_name:
+            if " ".join(names) == previous_name:
+                previous_name = ""
             continue
-        # Retrieving names and corresponding districts for all candidates
-        name = line.split(",")[0][1:-1]
-        district = line.split(",")[3][1:-1]
-        party = line.split(",")[5][1:-1]
-        # Launching a web search on DuckDuckGo name + district + Twitter (+ options language = Spanish, country = Spain)
-        page = pq(url = "https://duckduckgo.com/html/?q=" + name + " " + district + " twitter" + "&kl=es-es")
-# Retrieving results' links (href) (from Mozilla's option "Inspect element") 
-        links = page("a.large")
-        for link in links:
-            results = pq(link).attr("href")
-            # Keeping the first link containing "twitter.com"
-            if "twitter.com/" in results:
-                # If the link is a tweet (status), we only keep the account's address
-                if "/status/" in results:
-                    results = results.split("/status")[0]
-                # If the link is only the main Twitter page, we display an error
-                test_link = results.split("twitter.com/")[1]
-                if test_link == "":
-                    results = "No result found for " + name + " of " +  party + " in " + district
-                break
-            else:
-                results = "No result found for " + name + " of " +  party + " in " + district
-        print results
-        # Writing for each candidate the address or the error in the results file
-        accounts.write(('"%s"\n') % (results))
-        # Taking a brief break between each search in order not to be ejected by the website
-        time.sleep(3)
-    candidates.close()
-    accounts.close()
+        
+        # Separate first names and last names
+        separator = 2
+        if len(names) <= 2:
+            separator = 1
+        fname = names[:separator]
+        lname = names[separator:]
+    
+        # Generate combinations of first and last names
+        comb_fnames = allCombinations(fname)
+        comb_lnames = allCombinations(lname)
+        handle_list = []
+        for fnames in comb_fnames:
+            for lnames in comb_lnames:
+                print " ".join([fnames, lnames, district, party])
+                getTwitters_ddg(" ".join([fnames, lnames, district, party]), 10, handle_list)
+                getTwitters_Tw(" ".join([fnames, lnames]), 10, handle_list)
+                getTwitters_G(" ".join([fnames, lnames, district, party]), 10, handle_list)
+                # Taking a brief break between each search in order not to be ejected by the website
+                time.sleep(3)
+        accounts.write (('%s, %s\n') % ((" ".join(names)), handle_list))
+        print (" ".join(names))
 
-retrievePseudo()
+def cleanNames(names):
+# Remove particles and parenthesis in names
+    filtered_names = []
+    for word in names:
+        if len(word) and word[0].lower() != word[0]:
+            filtered_names.append(word)
+    return filtered_names
+    
+def allCombinations(tab):
+    out = []
+    for n in range(1, len(tab) + 1):
+        for comb in itertools.combinations(tab, n):
+            out.append(" ".join(comb))
+    return out
+
+# Retrieving candidates twitters from DuckDuckGo
+def getTwitters_ddg(query, nbAccounts, out):
+    site = "site:twitter.com"
+    lang = "es-es"
+    base = "https://duckduckgo.com/html/"
+    url = base + "?q=" + query + " " + site + "&kl=" + lang
+    page = pq(url = url)
+
+    counter = 0
+    links = page("a.large")
+    for link in links:
+        twitter_url = pq(link).attr("href")
+        if re.match('https?://twitter.com/[^/]+$', twitter_url):
+            handle = twitter_url.split('/')[-1]
+            if handle not in out:
+                out.append(handle)
+            # handle = handle.split("twitter.com/")[1]
+            counter += 1
+            if counter >= nbAccounts:
+                break
+    return out
+
+# Retrieving candidates twitters from Twitter
+def getTwitters_Tw(query, nbAccounts, out):
+    base = "https://twitter.com/search?f=users&vertical=default"
+    url = base + "&q=" + query + "&src=typd"
+    page = pq(url = url)
+    counter = 0
+    links = page("a.ProfileCard-screennameLink")
+    for link in links:
+        handle = pq(link).attr("href")
+        if "/" in handle:
+            handle = handle.split('/')[:-1]
+        if handle not in out:
+            out.append(handle)
+        counter += 1
+        if counter >= nbAccounts:
+            break
+    return out
+
+# Retrieving candidates twitters from Google
+def getTwitters_G(query, nbAccounts, out):
+    site = "site:twitter.com"
+    base = "https://encrypted.google.com/search?"
+    url = base + "q=" + query + " " + site
+    page = pq(url = url)
+    counter = 0
+    for link in page("cite"):
+        twitter_url = pq(link).text().replace(" ", "")
+        if re.match('https?://twitter.com/[^/]+$', twitter_url):
+            handle = twitter_url.split('/')[-1]
+            if '?' in handle:
+                handle = handle.split('?')[0]
+            if handle not in out:
+                out.append(handle)
+            counter += 1
+            if counter >= nbAccounts:
+                break
+    return out
+
+retrieveAccounts()
